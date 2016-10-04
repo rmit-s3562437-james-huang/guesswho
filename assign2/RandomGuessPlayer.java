5f601af041b9dd8a5e4aff3cf30e61610c09384b
rmit-s3426571-thong-nguyen <tom@home-2.local>
2016-10-04 11:55:25 +1100

    reformat

--- assign2/RandomGuessPlayer.java
+++ assign2/RandomGuessPlayer.java
@@ -4,26 +4,24 @@ import java.util.*;
 /**
  * Random guessing player.
  * This player is for task B.
- *
+ * <p>
  * You may implement/extend other interfaces or classes, but ensure ultimately
  * that this class implements the Player interface (directly or indirectly).
  */
-public class RandomGuessPlayer implements Player
-{
+public class RandomGuessPlayer implements Player {
     /**
      * Loads the game configuration from gameFilename, and also store the chosen
      * person.
      *
      * @param gameFilename Filename of game configuration.
-     * @param chosenName Name of the chosen person for this player.
+     * @param chosenName   Name of the chosen person for this player.
      * @throws IOException If there are IO issues with loading of gameFilename.
-     *    Note you can handle IOException within the constructor and remove
-     *    the "throws IOException" method specification, but make sure your
-     *    implementation exits gracefully if an IOException is thrown.
+     *                     Note you can handle IOException within the constructor and remove
+     *                     the "throws IOException" method specification, but make sure your
+     *                     implementation exits gracefully if an IOException is thrown.
      */
     public RandomGuessPlayer(String gameFilename, String chosenName)
-        throws IOException
-    {
+            throws IOException {
         // reads game file
         Scanner gameFileScan = new Scanner(new File(gameFilename));
 
@@ -33,16 +31,15 @@ public class RandomGuessPlayer implements Player
 
         PlayerFromFile chosenPlayer = null;
 
-        while (gameFileScan.hasNextLine()){
+        while (gameFileScan.hasNextLine()) {
             PlayerFromFile player = readPlayerFromFile(gameFileScan);
             players.add(player);
         }
 
-        for (PlayerFromFile player : players){
-            if (player.getName().equals(chosenName)){
+        for (PlayerFromFile player : players) {
+            if (player.getName().equals(chosenName)) {
                 chosenPlayer = new PlayerFromFile(player.getName(), player.getAttributes());
             }
-
         }
 
         System.out.println(chosenPlayer.getName() + chosenPlayer.getAttributes());
@@ -60,15 +57,15 @@ public class RandomGuessPlayer implements Player
 
     } // end of RandomGuessPlayer()
 
-    private Map<String, List<String>> readAttributes(Scanner gameFileScan){
+    private Map<String, List<String>> readAttributes(Scanner gameFileScan) {
         Map<String, List<String>> attributes = new HashMap<String, List<String>>();
 
-        while (gameFileScan.hasNextLine()){
+        while (gameFileScan.hasNextLine()) {
             //read line by line
             String line = gameFileScan.nextLine();
 
             //check newline
-            if (line.equals("")){
+            if (line.equals("")) {
                 break;
             }
 
@@ -85,7 +82,7 @@ public class RandomGuessPlayer implements Player
             List<String> values = new ArrayList<String>();
 
             //then put the rest in the arraylist as the values
-            while (lineScan.hasNext()){
+            while (lineScan.hasNext()) {
                 String value = lineScan.next();
                 values.add(value);
             }
@@ -94,14 +91,14 @@ public class RandomGuessPlayer implements Player
         return attributes;
     }
 
-    private PlayerFromFile readPlayerFromFile(Scanner gameFileScan){
+    private PlayerFromFile readPlayerFromFile(Scanner gameFileScan) {
         String name = "";
         Map<String, String> attributes = new HashMap<String, String>();
 
-        while (gameFileScan.hasNextLine()){
+        while (gameFileScan.hasNextLine()) {
             //read attribute and value of the P player
             String line = gameFileScan.nextLine();
-            if (line.equals("")){
+            if (line.equals("")) {
                 break;
             }
 
@@ -111,9 +108,9 @@ public class RandomGuessPlayer implements Player
 
             String[] keyValue = line.split(" ");
             //check whether it the players name or the attribute
-            if (keyValue.length > 1){
+            if (keyValue.length > 1) {
                 attributes.put(keyValue[0], keyValue[1]);
-            }else {
+            } else {
                 //vaue < 1 then must be the name
                 name = line;
             }
@@ -137,7 +134,7 @@ public class RandomGuessPlayer implements Player
     } // end of answer()
 
 
-	public boolean receiveAnswer(Guess currGuess, boolean answer) {
+    public boolean receiveAnswer(Guess currGuess, boolean answer) {
 
         // placeholder, replace
         return true;
@@ -145,19 +142,20 @@ public class RandomGuessPlayer implements Player
 
 } // end of class RandomGuessPlayer
 
-class PlayerFromFile{
+class PlayerFromFile {
     private String name;
     private Map<String, String> attributes;
 
-    PlayerFromFile(String name, Map<String, String> attributes){
+    PlayerFromFile(String name, Map<String, String> attributes) {
         this.name = name;
         this.attributes = attributes;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-    public Map<String, String> getAttributes(){
+    public Map<String, String> getAttributes() {
         return attributes;
     }
 
